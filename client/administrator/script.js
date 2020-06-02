5fa7f096819bb30754daf0cf7aabbb9c61a2ad42
Redon Osmanollaj <redon_osmanollaj@hotmail.com>
2020-06-02 15:51:11 +0200

    Admin post request

--- client/administrator/script.js
+++ client/administrator/script.js
@@ -9,9 +9,16 @@ fetch("http://134.122.87.241/api/teams")
         var option2 = document.createElement("option")
 
         option1.innerHTML = a.name
+        option1.value = a.id
         option2.innerHTML = a.name
+        option2.value = a.id
 
         selectHome.appendChild(option1)
         selectAway.appendChild(option2)
     })
 })
+
+function addMatch(event){
+    event.preventDefault();
+    alert(selectHome.options[selectHome.selectedIndex].value);
+}
