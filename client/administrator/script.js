18a8c5715a72180bcc9202450b8ddf713ca5fb43
lirimimeri <44481883+lirimimeri@users.noreply.github.com>
2020-06-12 15:08:29 +0200

    Finished: javascript/nodejs

--- client/administrator/script.js
+++ client/administrator/script.js
@@ -18,27 +18,46 @@ fetch("http://134.122.87.241/api/teams")
     })
 })
 
-// document.getElementById('addMatchForm').addEventListener('submit', (ev) => {
-//     home_score = home_score.value
-//     away_score = away_score.value
-//     home_id = home_id.value
-//     date = date.value
-//     week_id = week_id.value
-//     away_id = away_id.value
-//     fetch("http://134.122.87.241/api/matches", {
-//         method: 'POST',
-//         headers: {
-//             'Authentication': `Bearer ${sessionStorage.getItem('token')}`
-//         },
-//         body: JSON.stringify({
-//             'week_id': week_id,
-//             'date': date,
-//             'home_id': home_id,
-//             'home_score': home_score,
-//             'away_score': away_score,
-//             'away_id': away_id,
-//         })
-//     })
-//     .then(response => response.json())
-    
-// })
+$("#addMatchForm").submit(function (e) {
+    e.preventDefault();
+    validate()
+    try
+    {
+      fetch('http://134.122.87.241/api/matches',
+      {
+        method: 'POST',
+        headers: {
+          "Authorization": `Bearer ${sessionStorage.getItem("token")}`,
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "week_id": week_id,
+          "date": date,
+          "home_id": home_id,
+          "home_score": home_score,
+          "away_score": away_score,
+          "away_id": away_id
+        })
+      })
+      
+    }
+    catch(error){
+      alert(error)
+    }
+  });
+  function validate()
+  {
+    home_score = document.getElementById("home_score").value
+    away_score = document.getElementById("away_score").value
+    home_id = document.getElementById("select-home").value
+    date = document.getElementById("date").value
+    week_id = document.getElementById("week_id").value
+    away_id = document.getElementById("select-away").value
+
+    if(home_id === "" || away_id === "" || week_id === "" || home_score === "" || 
+      away_score === "" || date === "")
+    {
+        alert("*please fill all the parameters")
+    }
+  }
